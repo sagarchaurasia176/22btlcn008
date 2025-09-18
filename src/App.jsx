a3cdea31ee387b7a1c0ce33716a011e100e7ae0d
sagarchaurasia176 <sagarchaurasia176@gmail.com>
2025-09-18 10:22:47 +0530

    folder added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import './App.css';
+
+// app - main eiter
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element=""></Route>
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
